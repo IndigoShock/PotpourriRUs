a3e138147a8e9b7911505f58d552cdb4a230de4f
Collin Hintzke <collinhintzke5@gmail.com>
2018-07-11 15:22:49 -0700

    Added tag helpers

--- PuffyAmiYumi/Startup.cs
+++ PuffyAmiYumi/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,19 +17,26 @@ namespace PuffyAmiYumi
 {
     public class Startup
     {
-        public IConfiguration Configuration;
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddScoped<IInventory, DevInven>();
             services.AddDbContext<YumiDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("ProductionConnection")));
-        }
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -37,6 +45,9 @@ namespace PuffyAmiYumi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseAuthentication();
+            app.UseStaticFiles();
+
             app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
