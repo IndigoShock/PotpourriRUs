4eea028eed5cddbf03a2be5a468bd50d9d2171ed
stephendeanharper <stephendeanharper@gmail.com>
2018-07-16 07:49:28 -0700

    updated admin, products, iinventory and devinven, plus allproducts display

--- PuffyAmiYumi/Controllers/ProductsController.cs
+++ PuffyAmiYumi/Controllers/ProductsController.cs
@@ -1,10 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Xml;
+using PuffyAmiYumi.Models.Interfaces;
 
 namespace PuffyAmiYumi.Controllers
 {
+    [Authorize(Policy = "AdminOnly")]
     public class ProductsController : Controller
     {
-        public IActionResult Index()
+        private IInventory _context;
+
+        private readonly IConfiguration Configuration;
+
+        public ProductsController(IInventory context, IConfiguration configuration)
+        {
+            _context = context;
+            Configuration = configuration;
+        }
+
+        public IActionResult ViewAllProducts()
+        {
+            var products = _context.GetProduct();
+
+            return View(products);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
         {
             return View();
         }
